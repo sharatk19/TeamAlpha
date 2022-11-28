bd67d81e2fe6294f600ce5df18bf560e6803d9f9
tsanga18 <angus.tsang@mail.utoronto.ca>
2022-11-28 18:31:45 -0500

    Working on AiPlayer

---
+++ BattleShip/AiPlayer.java
@@ -0,0 +1,15 @@
+package BattleShip;
+
+import java.io.Serializable;
+
+public class AiPlayer implements Serializable{
+    boolean search = false;
+
+    public AiPlayer() {
+
+    }
+
+    public void bestMove(Board board) {
+
+    }
+}
--- BattleShip/Player.java
+++ BattleShip/Player.java
@@ -1,2 +1,4 @@
+package BattleShip;
+
 public class Player {
 }
