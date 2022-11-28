9cca679024dca15ba45bc2fbe09b9339cadc026f
tsanga18 <angus.tsang@mail.utoronto.ca>
2022-11-28 18:54:19 -0500

    Added Move class

--- BattleShip/AiPlayer.java
+++ BattleShip/AiPlayer.java
@@ -3,13 +3,5 @@ package BattleShip;
 import java.io.Serializable;
 
 public class AiPlayer implements Serializable{
-    boolean search = false;
 
-    public AiPlayer() {
-
-    }
-
-    public void bestMove(Board board) {
-
-    }
 }
