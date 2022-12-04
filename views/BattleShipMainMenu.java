8ad6ee33af77f3dcd4f11f4ccdfab6c03bf5c257
karimdarra <116992617+karimdarra@users.noreply.github.com>
2022-12-04 18:13:45 -0500

    getting the interface sorted, still need to link the buttons and link everything to the rest of the code will be done tonight

--- views/BattleShipMainMenu.java
+++ views/BattleShipMainMenu.java
@@ -1,4 +1,27 @@
 package views;
 
-public class BattleShipMainMenu {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class BattleShipMainMenu extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
