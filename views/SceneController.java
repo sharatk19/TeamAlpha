8e19440eeb336f4b8168f16fe151e2a4c621b2ff
karimdarra <116992617+karimdarra@users.noreply.github.com>
2022-12-06 07:29:47 -0500

    fully completed main menu and ui, implemented colorblindness for main menu, implemented audio music and color togglebuttons for menu screen

--- views/SceneController.java
+++ views/SceneController.java
@@ -2,12 +2,18 @@ package views;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class SceneController {
@@ -15,6 +21,31 @@ public class SceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    @FXML
+    public ToggleButton colorButton;
+
+    @FXML
+    public ToggleButton musicButton;
+
+    @FXML
+    public ToggleButton audioButton;
+
+    @FXML
+    private Label colortext;
+
+    @FXML
+    private Label musictext;
+
+    @FXML
+    private Label audiotext;
+
+    @FXML
+    private AnchorPane optionsBackground;
+
+    @FXML
+    private AnchorPane menuBackground;
+
 
     public void switchToMainMenu(ActionEvent event) throws IOException {
         URL url = getClass().getResource("MainMenu.fxml");
@@ -37,4 +68,47 @@ public class SceneController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void logout(ActionEvent event) throws IOException {
+
+        Node source = (Node)event.getSource();
+        stage = (Stage)source.getScene().getWindow();
+        stage.close();
+    }
+
+    // tie this to actually starting the game
+//    public void start(ActionEvent event) throws IOException {
+//
+//        Node source = (Node)event.getSource();
+//        stage = (Stage)source.getScene().getWindow();
+//    }
+
+    public void toggleButton(ActionEvent event) throws IOException {
+        if (event.getSource() == colorButton){
+            if (colorButton.isSelected()) {
+                colortext.setText("Color Blind Mode: On");
+//                colorBlindMode();
+            } else {
+                colortext.setText("Color Blind Mode: Off");
+            }
+        }
+        if (event.getSource() == musicButton){
+            if (musicButton.isSelected()) {
+                musictext.setText("Music: On");
+            } else {
+                musictext.setText("Music: Off");
+            }
+        }
+        if (event.getSource() == audioButton){
+            if (audioButton.isSelected()) {
+                audiotext.setText("Audio: On");
+            } else {
+                audiotext.setText("Audio: Off");
+            }
+        }
+    }
+//
+//    private void colorBlindMode() throws IOException {
+//        optionsBackground.getStylesheets().add()
+//    }
 }
