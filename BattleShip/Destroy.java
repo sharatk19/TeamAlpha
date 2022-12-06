b744d7ff596c18275093fef45ca9a98d926811db
Volkan <volkan.ozturk@mail.utoronto.ca>
2022-12-05 21:51:34 -0500

    Made Search not choose an already chosen coords.] Implemeneted executeMove in Destroy.java, but as it uses findTarget method, it is not complete.

--- BattleShip/Destroy.java
+++ BattleShip/Destroy.java
@@ -4,8 +4,28 @@ import java.util.ArrayList;
 
 public class Destroy implements Strategy{
 
+    private int recursionNumber= 0;
+
+
+    /*
+    The function finds which of the previous moves have hit enemy ship. Then sends the coords and other information to
+    findTarget helper method, which chooses where to attack second.
+     */
     @Override
     public int[] executeMove(Board board, ArrayList<Move> moves) {
-        return null;
+        for(Move target: moves){
+            if(target.isHit()){
+                return findTarget(board, moves, target);
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    /*
+    The function searches another hit move around the target. If it finds, it starts seaching linearly.
+     */
+    private int[] findTarget(Board board, ArrayList<Move> moves, Move target){
+        //TODO
+        return new int[0];
     }
 }
--- BattleShip/Search.java
+++ BattleShip/Search.java
@@ -4,10 +4,23 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Search implements Strategy{
+
+    /*
+    This function randomly selects a coordinate, then makes sure the coordinate is not already chosen
+    in another move.
+     */
     @Override
     public int[] executeMove(Board board, ArrayList<Move> moves) {
         Random r = new Random();
 
-        return new int[]{r.nextInt(10), r.nextInt(10)};
+        int[] result = new int[]{r.nextInt(10), r.nextInt(10)};
+
+        for(Move target: moves){
+            if(target.getCoord().equals(result)){
+                return executeMove(board, moves);
+            }
+        }
+
+        return result;
     }
 }
