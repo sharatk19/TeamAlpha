3c30b192b689f41426aadc506b2e1fad3ec24080
sharatk19 <sharat.krishnan@mail.utoronto.ca>
2022-12-07 11:03:18 -0500

    finished player

--- BattleShip/PlayerInterface.java
+++ BattleShip/PlayerInterface.java
@@ -10,15 +10,18 @@ import java.util.ArrayList;
 public class PlayerInterface {
 
     public ArrayList<Move> moves;
-    public Board[] player_board;
+    public Board player_board;
 
 
-    public PlayerInterface(Board[] player_board){
+    public PlayerInterface(Board player_board){
         this.moves = new ArrayList<>();
         this.player_board = player_board;
     }
 
     public void makeMove(Ship[] ships, int x, int y){
+        // Takes in Enemy Ships, and checks if Any Squares Hit
+
+
 
         // Make a move
 
@@ -31,6 +34,7 @@ public class PlayerInterface {
         for(Ship ship: ships){
             for(ShipSquare square: ship.getBody()){
                 if(square.x == x && square.y == y){
+                    this.player_board.testShot(x, y);
                     // Update the board at that square to True
                 }
             }
@@ -42,11 +46,12 @@ public class PlayerInterface {
     public void addMove(Move move){
         this.moves.add(move);
     }
-    public void setPlayerShips(Ship ship){
-//        for(Ship ship: ships){
-//            for(ShipSquare square: ship.getBody()){
-//                // call place piece on board but change call to ShipSquare instead of Ship
-//            }
-//        }
+    public void setPlayerShips(ArrayList<ShipSquare> shipsquares, String name){
+        Ship ship = new Ship(name, shipsquares.size());
+        ship.changeBody(shipsquares);
+
+        for(ShipSquare squares: ship.getBody()){
+            this.player_board.placeSquare(squares);
+        }
     }
 }
